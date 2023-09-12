31e30bf6282b21d6f4762784fcfba16bde83829d
Roknuzzaman <138369354+Roknuzzaman5546@users.noreply.github.com>
2023-09-12 13:03:05 +0600

    Create watchs.js

---
+++ watchs.js
@@ -0,0 +1,28 @@
+[
+    {
+      "Id": 1,
+      "brand": "casio",
+      "price": 1400
+    },
+    {
+      "Id": 2,
+      "brand": "conclick",
+      "price": 250
+    },
+    {
+      "Id": 3,
+      "brand": "mordan",
+      "price": 300
+    },
+    {
+      "Id": 4,
+      "brand": "range",
+      "price": 450
+    },
+    {
+      "Id": 5,
+      "brand": "float",
+      "price": 420
+    }
+  ]
+  
